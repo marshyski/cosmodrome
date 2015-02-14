4f649e9e12de120695eb123d1ac1ac946df794d3
marshyski <marshyski@gmail.com>
2015-02-14 10:39:25 -0800

    Added API reference docs and optional SSL support

--- __init__.py
+++ __init__.py
@@ -10,12 +10,14 @@ yamldir = 'data/'
 common = yamldir + 'common.yaml'
 
 config_yaml = yaml.load(file('config.yaml', 'r'))
-context = SSL.Context(SSL.SSLv23_METHOD)
-context.use_privatekey_file(config_yaml['key'])
-context.use_certificate_file(config_yaml['cert'])
-
-app = Flask(__name__, static_url_path = "")
-sslify = SSLify(app, subdomains=True)
+if config_yaml['key']:
+   context = SSL.Context(SSL.SSLv23_METHOD)
+   context.use_privatekey_file(config_yaml['key'])
+   context.use_certificate_file(config_yaml['cert'])
+   app = Flask(__name__, static_url_path = "")
+   sslify = SSLify(app, subdomains=True)
+else:
+   app = Flask(__name__, static_url_path = "")
 
 @app.errorhandler(404)
 def not_found(error):
@@ -25,8 +27,18 @@ def not_found(error):
 def bad_request(error):
     return make_response(jsonify({"error": "Unknown Environment, Missing YAML File"}), 400)
 
+@app.route('/', methods = ['GET'])
+def api_doc():
+    """API Endpoint Reference"""
+    func_list = {}
+    for rule in app.url_map.iter_rules():
+        if rule.endpoint != 'static':
+            func_list[rule.rule] = app.view_functions[rule.endpoint].__doc__
+    return jsonify(func_list)
+
 @app.route('/metadata/common', methods=["GET"])
 def common_metadata():
+    """Common Data for All Environments Defined"""
     if os.path.isfile(common):
         return str(yaml.load(file(common, 'r'))).replace(',', ',\n').replace('{', ' ').replace('}', '\n')
     else:
@@ -34,6 +46,7 @@ def common_metadata():
 
 @app.route('/metadata/env', methods=["GET"])
 def all_metadata():
+    """All Environment Data Defined By Incoming Request"""
     ipaddy = re.match(r'(\d+\.\d+)',request.remote_addr).groups()[0]
     yamlconfig = yamldir + ipaddy + '.yaml'
     if os.path.isfile(yamlconfig):
@@ -43,6 +56,7 @@ def all_metadata():
 
 @app.route('/metadata/<string:metadata>', methods=["GET"])
 def get_metadata(metadata):
+    """Requested Key Data by Defined Environment"""
     ipaddy = re.match(r'(\d+\.\d+)',request.remote_addr).groups()[0]
     yamlconfig = yamldir + ipaddy + '.yaml'
     if os.path.isfile(yamlconfig) and metadata in open(yamlconfig).read():
@@ -55,4 +69,7 @@ def get_metadata(metadata):
         abort(404)
 
 if __name__ == '__main__':
-    app.run(host = "0.0.0.0", port = config_yaml['port'], debug = False, ssl_context=context)
+    if config_yaml['key']:
+    	app.run(host = "0.0.0.0", port = config_yaml['port'], debug = False, ssl_context=context)
+    else:
+	app.run(host = "0.0.0.0", port = config_yaml['port'], debug = False)
